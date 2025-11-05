7c235b8efc210bf703afb90d81efb44944415846
David Youngworth <david@typedef.ai>
2025-11-05 15:45:52 -0800

    revert de agent fix, remove generated data, fix configs

--- target/compiled/medallion_reference/models/staging/sfdc/stg_sfdc__account.sql
+++
@@ -1,9 +0,0 @@
-select
-    cast(account_id as varchar) as account_id,
-    cast(parent_account_id as varchar) as parent_account_id,
-    cast(account_name as varchar) as account_name,
-    cast(geo as varchar) as geo,
-    cast(company_type as varchar) as company_type,
-    cast(tier as varchar) as tier,
-    cast(billing_country as varchar) as billing_country
-from DEMO_AGENTS_DAVID.staging.sfdc_account
